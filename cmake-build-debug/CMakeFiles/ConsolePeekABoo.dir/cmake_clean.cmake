f0f92a828e9bdf5e213efbf9c1b54d47fb3680cf
Ben Stone <spector.8500@gmail.com>
2019-12-31 11:23:25 -0500

    CLion Proj

---
+++ cmake-build-debug/CMakeFiles/ConsolePeekABoo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ConsolePeekABoo.dir/main.cpp.obj"
+  "ConsolePeekABoo.exe"
+  "ConsolePeekABoo.exe.manifest"
+  "ConsolePeekABoo.pdb"
+  "libConsolePeekABoo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ConsolePeekABoo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+    // draw 40 x 30 char console window
+    // wait every 3 seconds
+    // draw X at rand pos
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
